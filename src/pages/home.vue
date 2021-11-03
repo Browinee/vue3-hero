e5f1f3317fe728e339877a2f12777abefffcec66
justin <>
2021-11-03 21:55:49 +0800

    feat: add rate

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,9 +1,23 @@
-
 <template>
   <h1>App</h1>
-  <TodoList />
+  <TodoList/>
+
+  <Rate :value="3"></Rate>
+  <Rate :value="4" theme="red"></Rate>
+  <Rate :value="1" theme="green"></Rate>
+
+  <h1>您的評分是</h1>
+  <Rate :value="score" @update-rate="update">Test:</Rate>
 </template>
 
 <script setup>
-import TodoList from "../components/todoList/TodoList.vue";
+import {ref} from 'vue'
+
+const score = ref(3)
+console.log("sore", score)
+
+function update(num) {
+  score.value = num
+}
+
 </script>
